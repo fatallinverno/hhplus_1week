de7dda06d316f5cf486cb440c5c8167691209541
Linverno <fatal_linverno@naver.com>
2024-09-24 18:04:19 +0900

    Exception 및 리턴값 업데이트

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -18,32 +18,32 @@ public class PointController {
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}")
-    public UserPoint point(@PathVariable long id) {
+    public UserPoint point(@PathVariable long id) throws Exception {
         //return new UserPoint(0, 0, 0);
-        return pointService.getUserPoint(id);
+        return pointService.userPoint(id);
     }
 
     /**
      * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}/histories")
-    public List<PointHistory> history(@PathVariable long id) {
-        return pointService.getPointHistory(id);
+    public List<PointHistory> history(@PathVariable long id) throws Exception {
+        return pointService.pointHistory(id);
     }
 
     /**
      * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/charge")
-    public UserPoint charge(@PathVariable long id, @RequestBody long amount) {
-        return new UserPoint(0, 0, 0);
+    public UserPoint charge(@PathVariable long id, @RequestBody long amount) throws Exception {
+        return pointService.chargePoint(id, amount);
     }
 
     /**
      * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/use")
-    public UserPoint use(@PathVariable long id, @RequestBody long amount) {
-        return new UserPoint(0, 0, 0);
+    public UserPoint use(@PathVariable long id, @RequestBody long amount) throws Exception {
+        return pointService.usePoint(id, amount);
     }
 }
