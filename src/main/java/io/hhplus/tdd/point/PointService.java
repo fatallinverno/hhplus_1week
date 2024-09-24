614f3b1b962f9e64db065d0c84b79ef42711b266
Linverno <fatal_linverno@naver.com>
2024-09-24 18:04:29 +0900

    에러 사항 코드 수정

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.PointValidation;
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 
@@ -9,18 +10,51 @@ public class PointService {
 
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
+    private final PointValidation pointValidation;
 
-    public PointService(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
+    public PointService(UserPointTable userPointTable, PointHistoryTable pointHistoryTable, PointValidation pointValidation) {
         this.userPointTable = userPointTable;
         this.pointHistoryTable = pointHistoryTable;
+        this.pointValidation = pointValidation;
     }
 
-    public UserPoint getUserPoint(long userId) {
+    public UserPoint userPoint(long userId) throws Exception {
+        pointValidation.validateUserId(userId);
+
         return userPointTable.selectById(userId);
     }
 
-    public List<PointHistory> getPointHistory(long id) {
-        return pointHistoryTable.selectAllByUserId(id);
+    public List<PointHistory> pointHistory(long userId) throws Exception {
+        pointValidation.validateUserId(userId);
+
+        return pointHistoryTable.selectAllByUserId(userId);
+    }
+
+    public UserPoint chargePoint(long userId, long amount) throws Exception {
+//        pointValidation.validateUserId(userId);
+//        pointValidation.validateChargeAmount(amount);
+
+        UserPoint userInfo = userPoint(userId);
+        long updatePoint = userInfo.point() + amount;
+
+        UserPoint updateUserPoint = userPointTable.insertOrUpdate(userId, updatePoint);
+
+        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return updateUserPoint;
+    }
+
+    public UserPoint usePoint(long userId, long amount) throws Exception {
+//        pointValidation.validateUserId(userId);
+//        pointValidation.validateUseAmount(userId, amount);
+
+        UserPoint userInfo = userPoint(userId);
+        long updatePoint = userInfo.point() - amount;
+
+        UserPoint updateUserPoint = userPointTable.insertOrUpdate(userId, updatePoint);
+        pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
+
+        return updateUserPoint;
     }
 
 }
