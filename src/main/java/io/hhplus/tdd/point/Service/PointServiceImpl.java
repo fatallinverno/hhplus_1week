b0777ce03d3603e2dff8200c0a46e0292a14e8f5
Linverno <fatal_linverno@naver.com>
2024-09-26 17:37:59 +0900

    CompletableFuture로 이용한 동시성 제어 구현

--- src/main/java/io/hhplus/tdd/point/Service/PointServiceImpl.java
+++ src/main/java/io/hhplus/tdd/point/Service/PointServiceImpl.java
@@ -7,9 +7,12 @@ import io.hhplus.tdd.point.PointHistory;
 import io.hhplus.tdd.point.PointPolicy;
 import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
+@Service
 public class PointServiceImpl implements PointService {
 
     private final UserPointTable userPointTable;
@@ -17,7 +20,7 @@ public class PointServiceImpl implements PointService {
     private final PointValidation pointValidation;
     private final PointPolicy pointPolicy;
 
-    public PointServiceImpl(UserPointTable userPointTable, PointHistoryTable pointHistoryTable, PointValidation pointValidation, PointPolicy pointPolicy) {
+    public PointServiceImpl(PointValidation pointValidation, PointPolicy pointPolicy, UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
         this.userPointTable = userPointTable;
         this.pointHistoryTable = pointHistoryTable;
         this.pointValidation = pointValidation;
@@ -36,36 +39,36 @@ public class PointServiceImpl implements PointService {
         return pointHistoryTable.selectAllByUserId(userId);
     }
 
-    public UserPoint chargePoint(long userId, long amount) {
-        pointValidation.validateUserId(userId);
-        pointValidation.validateChargeAmount(amount);
+    public CompletableFuture<UserPoint> chargePointAsync(long userId, long amount) {
+        return CompletableFuture.supplyAsync(() -> {
+            pointValidation.validateChargeAmount(amount);
 
-        UserPoint userInfo = userPoint(userId);
-        long maxPoint = pointPolicy.getMaxPointLimit();
-        long updatePoint = userInfo.point() + amount;
+            UserPoint userInfo = userPoint(userId);
+            long maxPoint = pointPolicy.getMaxPointLimit();
+            long sumPoint = userInfo.point() + amount;
 
-        pointValidation.validateMaxPoint(updatePoint, maxPoint);
+            pointValidation.validateMaxPoint(sumPoint, maxPoint);
 
-        UserPoint updateUserPoint = userPointTable.insertOrUpdate(userId, updatePoint);
+            UserPoint updateUserPoint = userPointTable.insertOrUpdate(userId, sumPoint);
+            pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, 1L);
 
-        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, 1L);
-
-        return updateUserPoint;
+            return updateUserPoint;
+        });
     }
 
-    public UserPoint usePoint(long userId, long amount) {
-        pointValidation.validateUserId(userId);
+    public CompletableFuture<UserPoint> usePointAsync(long userId, long amount) {
+        return CompletableFuture.supplyAsync(() -> {
 
-        UserPoint userInfo = userPoint(userId);
+            UserPoint userInfo = userPoint(userId);
+            pointValidation.validateUseAmount(userInfo.point(), amount);
 
-        pointValidation.validateUseAmount(userInfo.point(), amount);
+            long updatePoint = userInfo.point() - amount;
 
-        long updatePoint = userInfo.point() - amount;
+            UserPoint updateUserPoint = userPointTable.insertOrUpdate(userId, updatePoint);
+            pointHistoryTable.insert(userId, amount, TransactionType.USE, 1L);
 
-        UserPoint updateUserPoint = userPointTable.insertOrUpdate(userId, updatePoint);
-        pointHistoryTable.insert(userId, amount, TransactionType.USE, 1L);
-
-        return updateUserPoint;
+            return updateUserPoint;
+        });
     }
 
 }
