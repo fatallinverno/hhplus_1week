ed772a61fbc0cc02ee943f6b38ed0bfe0d7cabcd
Linverno <fatal_linverno@naver.com>
2024-09-26 17:37:33 +0900

    에러사항 수정

--- src/main/java/io/hhplus/tdd/point/PointPolicy.java
+++ src/main/java/io/hhplus/tdd/point/PointPolicy.java
@@ -1,10 +1,13 @@
 package io.hhplus.tdd.point;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PointPolicy {
 
     private final long maxPointLimit;
 
-    public PointPolicy(long maxPointLimit) {
+    public PointPolicy() {
         this.maxPointLimit = 10000L;
     }
 
